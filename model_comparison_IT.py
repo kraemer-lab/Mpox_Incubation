fd3af2c82876fe3c544333a67ac3588487506acc
unknown <simon@BIO-W-DT--2605.zoo.ox.ac.uk>
2023-08-11 17:39:25 +0100

    add Bayesian workflow

--- model_comparison_IT.py
+++ model_comparison_IT.py
@@ -39,42 +39,95 @@ tSymptomOnset = df['onset'].values.astype("int")
 obs = tSymptomOnset - tStartExposure
 
 with pm.Model() as mod_l:
-    #u = pm.Gamma("u", ps_l['mean'][0], ps_l['sd'][0])
-    u = pm.Beta("u", 1, 1)
-    e = u * (tEndExposure - tStartExposure)
-    a = pm.Gamma("a", mu=ps_l['mean'][1], sigma=ps_l['sd'][1])
-    b = pm.Gamma("b", mu=ps_l['mean'][2], sigma=ps_l['sd'][2])
-    y = pm.LogNormal("y", mu=a+e, sigma=b, observed=obs)
-    idata_l = pm.sample(1000, idata_kwargs={"log_likelihood": True}, target_accept=0.99, random_seed=27)
+    r = pm.Beta("r", 1, 1)
+    e = r * (tEndExposure - tStartExposure)
+    m = pm.Gamma("m", mu=ps_l['mean'][1], sigma=ps_l['sd'][1])
+    s = pm.Gamma("s", mu=ps_l['mean'][2], sigma=ps_l['sd'][2])
+    y = pm.LogNormal("y", mu=m+e, sigma=s, observed=obs)
+    ppc_l = pm.sample_prior_predictive(1000) #prior predictives
 
 with pm.Model() as mod_g:
-    #u = pm.Gamma("u", ps_g['mean'][0], ps_g['sd'][0])
-    u = pm.Beta("u", 1, 1)
-    e = u * (tEndExposure - tStartExposure)
-    a = pm.Gamma("a", mu=ps_g['mean'][1], sigma=ps_g['sd'][1])
-    b = pm.Gamma("b", mu=ps_g['mean'][2], sigma=ps_g['sd'][2])
-    y = pm.Gamma("y", mu=a+e, sigma=b, observed=obs)
-    idata_g = pm.sample(1000, idata_kwargs={"log_likelihood": True}, random_seed=27)
+    r = pm.Beta("r", 1, 1)
+    e = r * (tEndExposure - tStartExposure)
+    m = pm.Gamma("m", mu=ps_g['mean'][1], sigma=ps_g['sd'][1])
+    s = pm.Gamma("s", mu=ps_g['mean'][2], sigma=ps_g['sd'][2])
+    y = pm.Gamma("y", mu=m+e, sigma=s, observed=obs)
+    ppc_g = pm.sample_prior_predictive(1000) #prior predictives
 
 with pm.Model() as mod_w:
-    #u = pm.Gamma("u", ps_w['mean'][0], ps_w['sd'][0])
-    u = pm.Beta("u", 1, 1)
-    e = u * (tEndExposure - tStartExposure)
-    a = pm.Gamma("a", mu=ps_w['mean'][1], sigma=ps_w['sd'][1])
-    b = pm.Gamma("b", mu=ps_w['mean'][2], sigma=ps_w['sd'][2])
-    y = pm.Weibull("y", alpha=a+e, beta=b, observed=obs)
-    idata_w = pm.sample(1000, idata_kwargs={"log_likelihood": True}, target_accept=0.99, random_seed=27)
+    r = pm.Beta("r", 1, 1)
+    e = r * (tEndExposure - tStartExposure)
+    m = pm.Gamma("m", mu=ps_w['mean'][1], sigma=ps_w['sd'][1])
+    s = pm.Gamma("s", mu=ps_w['mean'][2], sigma=ps_w['sd'][2])
+    y = pm.Weibull("y", alpha=m+e, beta=s, observed=obs)
+    ppc_w = pm.sample_prior_predictive(1000) #prior predictives
     
-with pm.Model() as mod_wa:
-    #u = pm.Gamma("u", ps_n['mean'][0], ps_n['sd'][0])
-    u = pm.Beta("u", 1, 1)
-    e = u * (tEndExposure - tStartExposure)
-    a = pm.Gamma("a", mu=ps_n['mean'][1], sigma=ps_n['sd'][1])
-    b = pm.Gamma("b", mu=ps_n['mean'][2], sigma=ps_n['sd'][2])
-    y = pm.NegativeBinomial("y", mu=a+e, alpha=b, observed=obs)
-    idata_n = pm.sample(1000, idata_kwargs={"log_likelihood": True}, random_seed=27)
+with pm.Model() as mod_n:
+    r = pm.Beta("r", 1, 1)
+    e = r * (tEndExposure - tStartExposure)
+    m = pm.Gamma("m", mu=ps_n['mean'][1], sigma=ps_n['sd'][1])
+    s = pm.Gamma("s", mu=ps_n['mean'][2], sigma=ps_n['sd'][2])
+    y = pm.NegativeBinomial("y", mu=m+e, alpha=s, observed=obs)
+    ppc_n = pm.sample_prior_predictive(1000) #prior predictives
 
+#plot priors
+colors = ["#44AA99", "k", "#AA4499"]
+ppcs = [ppc_l, ppc_g, ppc_w, ppc_n]
+ppc_names = ["LogNormal", "Gamma", "Weibull", "NegativeBinomial"]
+samps = np.random.randint(1000, size=100)
+for p in range(len(ppcs)):
+    y = az.extract(ppcs[p].prior_predictive)['y'].values
+    r = az.extract(ppcs[p].prior)['r'].values
+    m = az.extract(ppcs[p].prior)['m'].values
+    s = az.extract(ppcs[p].prior)['s'].values
+    fig, ax, = plt.subplots(2,2, figsize=(10,10))
+    ax[0,0].plot(np.arange(len(obs)), obs, color=colors[1], label="Observed")
+    for i in samps:
+        ax[0,0].plot(np.arange(len(obs)), y[:,i], alpha=0.1, color=colors[0])
+    ax[0,0].plot(np.nan, np.nan, color=colors[0], label="Prior predictive")
+    ax[0,0].plot(np.arange(len(obs)), y.mean(axis=1), linestyle="--", color=colors[2], label="Prior predictive mean")
+    ax[0,0].set_axisbelow(True)
+    ax[0,0].grid(0.1)
+    ax[0,0].legend(loc="upper right", fontsize=12)
+    ax[0,0].set_xlabel("Case")
+    ax[0,0].set_ylabel("Symptom Onset - Start Exposure")
+    ax[0,0].set_title("A. Predicted y")
+    ax[0,1].hist(r, bins=100, color="sienna", alpha=0.5)
+    ax[0,1].set_xlabel("Days")
+    ax[0,1].set_ylabel("Density")
+    ax[0,1].set_title("B. Prior r")
+    ax[0,1].set_axisbelow(True)
+    ax[0,1].grid(0.1)
+    ax[1,0].hist(m, bins=100, color="orangered", alpha=0.5)
+    ax[1,0].set_xlabel("Days")
+    ax[1,0].set_ylabel("Density")
+    ax[1,0].set_title("C. Prior m")
+    ax[1,0].set_axisbelow(True)
+    ax[1,0].grid(0.1)
+    ax[1,1].hist(s, bins=100, color="slateblue", alpha=0.5)
+    ax[1,1].set_xlabel("Days")
+    ax[1,1].set_ylabel("Density")
+    ax[1,1].set_title("D. Prior s")
+    ax[1,1].set_axisbelow(True)
+    ax[1,1].grid(0.1)
+    plt.suptitle(ppc_names[p]+" Priors (Italy)")
+    plt.tight_layout()
+    plt.savefig("./prior_predictive/IT_priors_"+ppc_names[p]+".png", dpi=600)
+    
 
+###### sample models #########
+with mod_l:
+    idata_l = pm.sample(1000, idata_kwargs={"log_likelihood": True}, target_accept=0.99, random_seed=27) #NUTS sampler
+
+with mod_g:
+    idata_g = pm.sample(1000, idata_kwargs={"log_likelihood": True}, random_seed=27) #NUTS sampler
+
+with mod_w:
+    idata_w = pm.sample(1000, idata_kwargs={"log_likelihood": True}, target_accept=0.99, random_seed=27) #NUTS sampler
+
+with mod_n:
+    idata_n = pm.sample(1000, idata_kwargs={"log_likelihood": True}, random_seed=27) #NUTS sampler
+    
 ###compare loo
 mods = {"LN":idata_l, "Gamma":idata_g, "Weibull":idata_w,  "NB":idata_n}
 loo = az.compare(mods, ic='loo')
@@ -109,22 +162,22 @@ loo_df = pd.DataFrame(loo)
 loo_df.to_csv("./summaries/IT_model_comp_waic.csv")
 
 
-pos_l_a = az.extract(idata_l.posterior)['a'].values
-pos_l_b = az.extract(idata_l.posterior)['b'].values
+pos_l_a = az.extract(idata_l.posterior)['m'].values
+pos_l_b = az.extract(idata_l.posterior)['s'].values
 means_l = np.exp(pos_l_a + (pos_l_b**2)/2)
 
-pos_g_a = az.extract(idata_g.posterior)['a'].values
-pos_g_b = az.extract(idata_g.posterior)['b'].values
+pos_g_a = az.extract(idata_g.posterior)['m'].values
+pos_g_b = az.extract(idata_g.posterior)['s'].values
 ag = (pos_g_a**2)/(pos_g_b**2)
 bg = pos_g_a/(pos_g_b**2)
 means_g = ag / bg
 
-pos_w_a = az.extract(idata_w.posterior)['a'].values
-pos_w_b = az.extract(idata_w.posterior)['b'].values
+pos_w_a = az.extract(idata_w.posterior)['m'].values
+pos_w_b = az.extract(idata_w.posterior)['s'].values
 means_w = pos_w_b*gamma(1+(1/pos_w_a))
 
-pos_n_a = az.extract(idata_n.posterior)['a'].values
-pos_n_b = az.extract(idata_n.posterior)['b'].values
+pos_n_a = az.extract(idata_n.posterior)['m'].values
+pos_n_b = az.extract(idata_n.posterior)['s'].values
 means_n = pos_n_a
 
 mod_names = ['LogNormal','Gamma','Weibull','NegativeBinomial']
@@ -204,44 +257,42 @@ inc_day = ((tSymptomOnset-tEndExposure)+(tSymptomOnset-tStartExposure))/2
 l = np.round(((30-18)/2), 0)
 r = 30-18 - l
 
-#inc_day = list(np.repeat(min(inc_day), l)) + list(inc_day) + list(np.repeat(max(inc_day), r))
-
 num_bins = x.shape[0]
 counts, bin_edges = np.histogram(np.sort(inc_day), bins=num_bins, density=True)
 e_cdf = np.cumsum(counts/counts.sum())
 
 fig, ax = plt.subplots(2,2, figsize=(10,10))
-sns.ecdfplot(inc_day, ax=ax[0,0], color='k', linestyle=":", label="Empirical CDF")
-ax[0,0].plot(x.mean(axis=1), l_cdf_m, color="slateblue", label="LogNormal CDF")
-ax[0,0].fill_between(x.mean(axis=1), l_cdf_5, l_cdf_95, color="slateblue", alpha=0.2, label="95% HDI")
-ax[0,0].set_ylabel("Proporton")
+sns.ecdfplot(inc_day, ax=ax[0,0], color='k',  label="Empirical CDF")
+ax[0,0].plot(x.mean(axis=1), l_cdf_m, color="#0072B2", linestyle="--", label="Posterior mean")
+ax[0,0].fill_between(x.mean(axis=1), l_cdf_5, l_cdf_95, color="#56B4E9", alpha=0.3, label="95% HDI")
+ax[0,0].set_ylabel("Proportion")
 ax[0,0].set_xlabel("Incubation period (days)")
 ax[0,0].spines[['right', 'top']].set_visible(False)
 ax[0,0].legend(loc="lower right")
 ax[0,0].grid(alpha=0.2)
 ax[0,0].set_title("A. LogNormal")
-sns.ecdfplot(inc_day, ax=ax[0,1], color='k', linestyle=":", label="Empirical CDF")
-ax[0,1].plot(x.mean(axis=1), g_cdf_m, color="crimson", label="Gamma CDF")
-ax[0,1].fill_between(x.mean(axis=1), g_cdf_5, g_cdf_95, color="crimson", alpha=0.2, label="95% HDI")
-ax[0,1].set_ylabel("Proporton")
+sns.ecdfplot(inc_day, ax=ax[0,1], color='k', label="Empirical CDF")
+ax[0,1].plot(x.mean(axis=1), g_cdf_m, color="#D55E00", linestyle="--", label="Posterior mean")
+ax[0,1].fill_between(x.mean(axis=1), g_cdf_5, g_cdf_95, color="#CC79A7", alpha=0.3, label="95% HDI")
+ax[0,1].set_ylabel("Proportion")
 ax[0,1].set_xlabel("Incubation period (days)")
 ax[0,1].spines[['right', 'top']].set_visible(False)
 ax[0,1].legend(loc="lower right")
 ax[0,1].grid(alpha=0.2)
 ax[0,1].set_title("B. Gamma")
-sns.ecdfplot(inc_day, ax=ax[1,0], color='k', linestyle=":", label="Empirical CDF")
-ax[1,0].plot(x.mean(axis=1), w_cdf_m, color="orange", label="Weibull CDF")
-ax[1,0].fill_between(x.mean(axis=1), w_cdf_5, w_cdf_95, color="orange", alpha=0.2, label="95% HDI")
-ax[1,0].set_ylabel("Proporton")
+sns.ecdfplot(inc_day, ax=ax[1,0], color='k', label="Empirical CDF")
+ax[1,0].plot(x.mean(axis=1), w_cdf_m, color="#E69F00", linestyle="--", label="Posterior mean")
+ax[1,0].fill_between(x.mean(axis=1), w_cdf_5, w_cdf_95, color="#F0E442", alpha=0.3, label="95% HDI")
+ax[1,0].set_ylabel("Proportion")
 ax[1,0].set_xlabel("Incubation period (days)")
 ax[1,0].spines[['right', 'top']].set_visible(False)
 ax[1,0].legend(loc="lower right")
 ax[1,0].grid(alpha=0.2)
 ax[1,0].set_title("C. Weibull")
-sns.ecdfplot(inc_day, ax=ax[1,1], color='k', linestyle=":", label="Empirical CDF")
-ax[1,1].plot(x.mean(axis=1), n_cdf_m, color="forestgreen", label="NegativeBinomial CDF")
-ax[1,1].fill_between(x.mean(axis=1), n_cdf_5, n_cdf_95, color="forestgreen", alpha=0.2, label="95% HDI")
-ax[1,1].set_ylabel("Proporton")
+sns.ecdfplot(inc_day, ax=ax[1,1], color='k', label="Empirical CDF")
+ax[1,1].plot(x.mean(axis=1), n_cdf_m, color="#117733", linestyle="--", label="Posterior mean")
+ax[1,1].fill_between(x.mean(axis=1), n_cdf_5, n_cdf_95, color="#44AA99", alpha=0.3, label="95% HDI")
+ax[1,1].set_ylabel("Proportion")
 ax[1,1].set_xlabel("Incubation period (days)")
 ax[1,1].spines[['right', 'top']].set_visible(False)
 ax[1,1].legend(loc="lower right")
@@ -301,7 +352,56 @@ plt.suptitle("Weibull (Italy)")
 plt.tight_layout()
 plt.savefig("./summary_plots/IT_weibull_rankplot.png", dpi=300)
 
-az.plot_trace(idata_l, kind="rank_vlines")
+az.plot_trace(idata_n, kind="rank_vlines")
 plt.suptitle("NegativeBinomial (Italy)")
 plt.tight_layout()
 plt.savefig("./summary_plots/IT_negativebinom_rankplot.png", dpi=300)
+
+
+
+########### Posterior predictive checks #################
+
+pred_l = pm.sample_posterior_predictive(idata_l, model=mod_l, random_seed=27) #osterior predictives
+
+pred_g = pm.sample_posterior_predictive(idata_g, model=mod_g, random_seed=27) #posterior predictives
+
+pred_w = pm.sample_posterior_predictive(idata_w, model=mod_w, random_seed=27) #posterior predictives
+
+pred_n = pm.sample_posterior_predictive(idata_n, model=mod_n, random_seed=27) #posterior predictives
+
+
+#plot posterior predictions
+ppcs = [pred_l, pred_g, pred_w, pred_n]
+ppc_names = ["LogNormal", "Gamma", "Weibull", "NegativeBinomial"]
+letters=["A. ", "B. ", "C. ", "D. "]
+colors = ["#44AA99", "k", "#AA4499"]
+fig, ax = plt.subplots(2,2, figsize=(10,10))
+axes = [(0,0), (0,1), (1,0), (1,1)]
+for p in range(len(ppcs)):
+    az.plot_ppc(ppcs[p], kind="cumulative", colors=colors, ax=ax[axes[p]], random_seed=27)
+    ax[axes[p]].set_title(letters[p]+ppc_names[p])
+    ax[axes[p]].spines[['right', 'top']].set_visible(False)
+    ax[axes[p]].set_xlabel("y (days)")
+    ax[axes[p]].set_ylabel("Proportion")
+    ax[axes[p]].grid(0.2)
+plt.suptitle("Posterior Predictives (Italy)")
+plt.tight_layout()
+plt.savefig("./posterior_predictive/IT_predictions.png", dpi=600)
+
+# ppcs = [pred_l, pred_g, pred_w, pred_n]
+# ppc_names = ["LogNormal", "Gamma", "Weibull", "NegativeBinomial"]
+# letters=["A. ", "B. ", "C. ", "D. "]
+# colors = [["#56B4E9", "k", "#0072B2"], ["#CC79A7", "k", "#D55E00"],
+#           ["#F0E442", "k", "#E69F00"], ["#44AA99", "k", "#117733"] ]
+# fig, ax = plt.subplots(2,2, figsize=(10,10))
+# axes = [(0,0), (0,1), (1,0), (1,1)]
+# for p in range(len(ppcs)):
+#     az.plot_ppc(ppcs[p], kind="cumulative", colors=colors[p], ax=ax[axes[p]], random_seed=27)
+#     ax[axes[p]].set_title(letters[p]+ppc_names[p])
+#     ax[axes[p]].spines[['right', 'top']].set_visible(False)
+#     ax[axes[p]].set_xlabel("y (days)")
+#     ax[axes[p]].set_ylabel("Proportion")
+#     ax[axes[p]].grid(0.2)
+# plt.suptitle("Posterior Predictives (Italy)")
+# plt.tight_layout()
+# plt.savefig("./posterior_predictive/IT_predictions.png", dpi=600)
